ab6b3f63b5c98dac5577861fa6dc8ae03619b7b5
gabrielmendezsoares <gabrielmendezsoares@gmail.com>
2025-08-20 15:03:49 -0300

    feat: 3.0.1

--- src/services/interfaces/index.ts
+++ src/services/interfaces/index.ts
@@ -1,5 +1,3 @@
-export * as IQuery from './IQuery.js';
-export * as IQueryData from './IQueryData.js';
 export * as IReqBody from './IReqBody.js';
 export * as IResponse from './IResponse.js';
 export * as IResponseData from './IResponseData.js';
